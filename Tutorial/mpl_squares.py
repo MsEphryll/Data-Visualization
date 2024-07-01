b40fb52f0068cdc93fe2b6583439c94447ced134
Eprhyll <ephryllbitang13@gmail.com>
2024-07-01 18:03:06 +0800

    Updated mpl_squares.py to add input_values, changed plot data, added style, and reformatted comments.

--- Tutorial/mpl_squares.py
+++ Tutorial/mpl_squares.py
@@ -2,23 +2,29 @@
 # pyplot module --> contains a number of functions that generate charts and plots.
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1,4,9,16,26]
 
 
 # subplots() --> can generate one or more plots in the same figure.
 # fig --> represents the entire figure or collection of plots that are generated.
 # ax --> represents the individual plot or subplots. Most used variable ta oll times.
+plt.style.use('ggplot')
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+
+# linewidth --> controls the thickness of the line that plot() generates.
+ax.plot(input_values, squares, linewidth=3)
 
 
 # Set chart title and label axes
-ax.set_title("Square Numbers", fontsize=24)
+ax.set_title("Square Numbers", fontsize=24)  # sets a title for the chart
+
+# allows to set title for each axes
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels
-ax.tick_params(axis='both', labelsize=14)
+ax.tick_params(axis='both', labelsize=14) # styles the tick marks
 
 
 # open's Matplotlibb viewer and displays the plot
